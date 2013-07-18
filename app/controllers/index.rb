f269dbeee1b59de9554f3319ec66f3203d2d9f1e
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-17 19:07:52 -0500

    cleanup work
    added a fragment _nav and cleaned up a bit

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,25 +1,21 @@
 get '/' do
-  p params
-  #Url.create(:long_url => params[:long_url])
-  # Main User Interface
   erb :index
 end
 
-get '/url/new_url' do
-  @long_url = Url.create(params[:long_url])
-  erb :new_url
+get '/urls' do
+  # show all links by user is next here
+  @urls = Url.all
+  erb :urls
 end
 
 get '/user' do
-  # Url.all(:long_url)
   # CRUD user data
-  # show all links
   erb :user
 end
 
 post '/' do
   @long_url = Url.create(:long_url => params[:long_url])
-  #logic for short url!
   #validate url
-  redirect to("/user?url=#{params[:long_url]}")
+  #logic for short url!
+  redirect to("/user?long_url=#{params[:long_url]}")
 end
